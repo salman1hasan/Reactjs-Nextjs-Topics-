0906f8ad4f231620f449eed3d257bc1b70e43e5b
Salman Hasan <96196317+salman1hasan@users.noreply.github.com>
2023-01-04 07:37:34 -0500

    Add files via upload

---
+++ Studying Notes/Reactjsauthentication.js
@@ -0,0 +1,32 @@
+// What is NodeJS?
+
+// We wanted to use javascript to make websites and web browser and we can use Nodejs as a backend app
+// Web server has a client and a server, If you have one request and response you’re looking at a fast response but if you have like 10 servers then theres not a lot
+// 10 requests = 10 threads, each javascript server is a thread [Tomcat provides us with 200 threads] can only have that many clients
+// Webserver used with NodeJS- Client and a request and sends it to another server. Nodejs uses a package of. It revolutionized coding because a person can write a server in a single language
+// It’s a way to write content in a server where it can previously be written only in a web server. It uses a non-blocking IO
+
+//What is express js?
+
+//Express is a NodeJS framework designed for building api's, web applications, and cross-platform mobile apps
+//it's high performance
+//Used as a server side scripting language
+//Creating expressjs is to speed up the process
+//Express js can save a lot of time
+//Middleware is an express handler which have the access to the applications request-response cycle
+//Routing, refers to how an applications endpoints respond to requests
+
+//Templating
+//Create a template file with less code and render html files
+
+//Debugging
+//Express makes it easier as it identifies the exact part where bugs are
+
+//What is mongoose
+//It's a way to connect mongodb and connect nodejs together. mongo db is a database and and it creates schema api/middlewares and much more
+
+//What is a schema
+//Structure of the data and what the information that we're going to be using
+
+//What is a mongoose model
+//its a wrap around for the mongoose model and schema.
